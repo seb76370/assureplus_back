199fb6bf7bee2304d5f82bab88ff8bb7e27ac76b
sebastien baudet <seb.baudet76@gmail.com>
2023-04-18 23:38:58 +0200

    ajout url server whitelist

--- src/assureplus_back/settings.py
+++ src/assureplus_back/settings.py
@@ -39,12 +39,7 @@ CORS_ALLOW_HEADERS = ['*']
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:4200',
-    'http://127.0.0.1:4200',
-    'https://dev-passion76.fr',
-    'https://dev-passion76.fr/assureplus/',
-]
+CORS_ORIGIN_WHITELIST = ['*']
 # os.environ['HTTPS'] = "on"
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
