05403e25103c09b6f8717caa9ffe4206bc25001f
sebastien baudet <seb.baudet76@gmail.com>
2023-04-18 23:24:28 +0200

    remove https options

--- src/assureplus_back/settings.py
+++ src/assureplus_back/settings.py
@@ -43,10 +43,10 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:4200',
     'http://127.0.0.1:4200',
 ]
-os.environ['HTTPS'] = "on"
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+# os.environ['HTTPS'] = "on"
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
 
 # Application definition
 
